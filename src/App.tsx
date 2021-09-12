819bbf1005ee15c5e217d134f4b9534ef0fc8a38
Truong Hoang <hqtruong27@gmail.com>
2021-09-13 01:20:00 +0700

    api get countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, MouseEventHandler, useEffect, useState } from 'react';
 import Country from './components/countries';
 import Highlight from './components/highlight';
 import Summary from './components/summary';
+import CovidApi from './components/api/index';
 
 function App() {
+
+  const [countries, setCountries] = useState<[]>([])
+
+  useEffect(() => {
+    CovidApi.countries().then(response => {
+      setCountries(response.data)
+    }).catch(error => console.log(error))
+  }, [])
+
+  const handleOnChange = (e: React.MouseEvent<HTMLLabelElement, MouseEvent>): void => {
+
+  }
+
   return (
     <>
-      <Country />
+      <Country value={''} countries={countries} onChange={handleOnChange} />
       <Summary />
       <Highlight />
     </>
